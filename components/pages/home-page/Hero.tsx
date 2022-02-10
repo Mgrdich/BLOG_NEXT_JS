f1aa349272f136c8851d1601d9150eb43e17f68b
Mgrdich <mgotm13@gmail.com>
2022-02-10 23:34:10 +0400

    Adding Styled components to the Hero component

--- components/pages/home-page/Hero.tsx
+++ components/pages/home-page/Hero.tsx
@@ -1,9 +1,49 @@
 import {FC} from 'react';
+import styled from "styled-components";
 
-const Hero:FC = () => {
+const HeroCmp = styled.div`
+  text-align: center;
+  background-image: linear-gradient(to bottom,
+  var(--color-grey-900),
+  var(--color-grey-800));
+  padding: var(--size-8) 0;
+`;
+
+const ImageContainer = styled.div`
+  width: 300px;
+  height: 300px;
+  box-shadow: 0 1px 8px rgba(0, 0, 0, 0.2);
+  border-radius: 50%;
+  overflow: hidden;
+  background-color: var(--color-grey-700);
+  margin: auto;
+`;
+
+const Image = styled.image`
+  object-fit: cover;
+  object-position: top;
+  width: 100%;
+  height: 100%;
+`;
+
+const Header = styled.h1`
+  font-size: var(--size-16);
+  margin: var(--size-4) 0;
+  color: var(--color-grey-300);
+`;
+
+const Paragraph = styled.p`
+  font-size: var(--size-6);
+  color: var(--color-grey-200);
+  width: 90%;
+  max-width: 40rem;
+  margin: auto;
+`;
+
+const Hero: FC = () => {
     return (
         <>
-
+            <HeroCmp/>
         </>
     );
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import '../styles/index.css';
 import type {AppProps} from 'next/app'
 import {ReactNode} from "react";
 
