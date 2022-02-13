aee549ede37af4c19ee7af6f0ced014cf2a1b490
Mgrdich <mgotm13@gmail.com>
2022-02-13 17:56:43 +0400

    Main Navigation Styling and Logo minor code improvements

--- components/layout/Logo.tsx
+++ components/layout/Logo.tsx
@@ -1,13 +1,26 @@
 import {FC} from 'react';
 import styled from "styled-components";
+import Link from "next/link";
 
 const StyledText = styled.div`
-
+  text-transform: uppercase;
+  font-size: var(--size-5);
+  font-weight: bold;
+  font-family: 'Oswald', sans-serif;
+  cursor: pointer;
+  color: var(--color-grey-50);
+  @media (min-width: 768px) {
+    font-size: var(--size-8);
+  }
 `;
 
 const Logo: FC = () => {
     return (
-        <StyledText>MGO Website</StyledText>
+        <Link href="/">
+                <StyledText>
+                    MGO Website
+                </StyledText>
+        </Link>
     );
 };
 
--- components/layout/MainNavigation.tsx
+++ components/layout/MainNavigation.tsx
@@ -3,24 +3,59 @@ import Logo from "./Logo";
 import Link from "next/link";
 import styled from "styled-components";
 
+const StyledHeader = styled.header`
+  height: 6rem;
+  background-color: var(--color-grey-900);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10%;
+
+  a {
+    color: var(--color-grey-100);
+    font-size: var(--size-4);
+
+    &:hover, &:active, &.active {
+      color: var(--color-grey-200);
+    }
+
+    @media (min-width: 768px) {
+      font-size: var(--size-5);
+    }
+  }
+`;
+
+const StyledUl = styled.ul`
+  list-style: none;
+  display: flex;
+  align-items: baseline;
+  margin: 0;
+  padding: 0;
+`;
+
+const StyledLi = styled.li`
+  margin: 0 var(--size-4);
+`;
 
 const MainNavigation: FC = () => {
     return (
-        <header>
-            <Link href="/">
-                <a><Logo/></a>
-            </Link>
+        <StyledHeader>
+            <Logo/>
             <nav>
-                <ul>
-                    <li>
-                        <Link href="/posts">Posts</Link>
-                    </li>
-                    <li>
-                        <Link href="/contact">Contact</Link>
-                    </li>
-                </ul>
+                <StyledUl>
+                    <StyledLi>
+                        <Link href="/posts">
+                            Posts
+                        </Link>
+                    </StyledLi>
+                    <StyledLi>
+                        <Link href="/contact">
+                            Contact
+                        </Link>
+                    </StyledLi>
+                </StyledUl>
             </nav>
-        </header>
+        </StyledHeader>
     );
 };
 
