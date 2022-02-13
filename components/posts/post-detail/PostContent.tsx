dd8f7a755c4a1626c9cc622cfee36c493ba265b2
Mgrdich <mgotm13@gmail.com>
2022-02-13 19:40:05 +0400

    Adding PostHeader and PostContent

--- components/posts/post-detail/PostContent.tsx
+++ components/posts/post-detail/PostContent.tsx
@@ -1,4 +1,31 @@
 import {FC} from 'react';
+import styled from "styled-components";
+
+
+const StyledContent = styled.div`
+  width: 95%;
+  max-width: 60rem;
+  margin: var(--size-8) auto;
+  font-size: var(--size-5);
+  line-height: var(--size-8);
+  background-color: var(--color-grey-100);
+  border-radius: 6px;
+  padding: var(--size-4);
+  @media (min-width: 768px) {
+    padding: var(--size-8);
+  }
+`;
+
+const StyledParagraph = styled.p`
+  color: var(--color-grey-800);
+`;
+
+const StyledImageCont = styled.div`
+  margin: var(--size-4) auto;
+  width: 100%;
+  max-width: 600px;
+`;
+
 
 const PostContent: FC = () => {
     return (
--- components/posts/post-detail/PostHeader.tsx
+++ components/posts/post-detail/PostHeader.tsx
@@ -1,6 +1,43 @@
 import {FC} from 'react';
+import styled from "styled-components";
 
-const PostHeader:FC = () => {
+const StyledHeader = styled.div`
+  padding-bottom: var(--size-8);
+  border-bottom: 8px solid var(--color-primary-100);
+  margin: var(--size-4) 0;
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: space-between;
+  align-items: center;
+  gap: 1rem;
+
+  img {
+    object-fit: cover;
+    width: 200px;
+    height: 120px;
+  }
+
+  @media (min-width: 768px) {
+    margin: var(--size-8) 0;
+    flex-direction: row;
+    align-items: flex-end;
+  }
+`;
+
+const StyledH1 = styled.h1`
+  font-size: var(--size-8);
+  color: var(--color-primary-500);
+  margin: 0;
+  line-height: initial;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    font-size: var(--size-16);
+    text-align: left;
+  }
+`;
+
+const PostHeader: FC = () => {
     return (
         <>
 
