a145b2a8347342f8115999a5d017280fdc1ab8a6
Mgrdich <mgotm13@gmail.com>
2022-02-16 23:58:20 +0400

    Dynamic Form Code initializations

---
+++ components/UI/DynamicForm.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface IDynamicForm {
+    formData: string[]
+}
+
+const DynamicForm:React.FC<IDynamicForm> = () => {
+    return (
+        <>
+
+        </>
+    );
+};
+
+export default DynamicForm;
