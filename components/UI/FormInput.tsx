2ae216aac8e5abd7f9116559f7d6bac3700bd313
Mgrdich <mgotm13@gmail.com>
2022-02-19 00:21:19 +0400

    Typing for formElements

--- components/UI/FormInput.tsx
+++ components/UI/FormInput.tsx
@@ -1,13 +1,16 @@
-import {FC} from 'react';
 import FormElement from "./FormElement";
 import Input from "./Input";
 import {IFormElement} from "../../types/components";
+import {ReactNode} from "react";
 
-interface IFormInput extends IFormElement{
+interface IFormInput<T> extends IFormElement<T> {}
 
-}
-
-const FormInput: FC<IFormInput> = ({label, id, name}) => {
+const FormInput = <TFormValues extends Record<string, unknown>>
+({
+     label,
+     id,
+     name
+ }: IFormInput<TFormValues>): ReactNode => {
     return (
         <FormElement label={label}>
             <Input id={id} name={name}/>
--- components/UI/FormTextarea.tsx
+++ components/UI/FormTextarea.tsx
@@ -1,13 +1,16 @@
-import {FC} from 'react';
 import FormElement from "./FormElement";
 import {IFormElement} from "../../types/components";
 import Textarea from "./Textarea";
 
 
-interface IFormTextarea extends IFormElement {
-}
+interface IFormTextarea<T> extends IFormElement<T> {}
 
-const FormTextarea: FC<IFormTextarea> = ({label, id, name}) => {
+const FormTextarea = <TFormValues extends Record<string, unknown>>
+({
+     label,
+     id,
+     name
+ }: IFormTextarea<TFormValues>) => {
     return (
         <FormElement label={label}>
             <Textarea id={id} name={name}/>
