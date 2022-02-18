379b8eb2222bf32a348d2df312bf9036b541b972
Mgrdich <mgotm13@gmail.com>
2022-02-19 01:05:40 +0400

    adding Global Styled components Flex container

--- styled/global.ts
+++ styled/global.ts
@@ -15,6 +15,7 @@ export const StyledInput = styled.input`
   border: 1px solid var(--color-grey-400);
   background-color: var(--color-grey-50);
   resize: none;
+  max-width: 100%;
 `;
 
 export const StyledTextArea = styled.textarea(StyledInput);
@@ -35,4 +36,48 @@ export const StyledError = styled.label`
   position: absolute;
   color: red;
   font-size: 14px;
+`;
+
+// TODO the string type
+interface IFlexContainer {
+    width: number;
+    flexDirection: string;
+    justifyContent: string;
+    alignItems: string;
+    maxWidth: number;
+    flexWrap: string;
+    flexGrow: number;
+    padding: number;
+    margin: number;
+    cursor: number;
+}
+
+export const FlexContainer = styled.div<IFlexContainer>`
+  display: flex;
+  width: ${({width}) => width};
+  flex-direction: ${({flexDirection}) => flexDirection || 'row'};
+  justify-content: ${({justifyContent}) => justifyContent};
+  align-items: ${({alignItems}) => alignItems};
+  max-width: ${({maxWidth}) => maxWidth};
+  flex-wrap: ${({flexWrap}) => (flexWrap ? 'wrap' : 'nowrap')};
+  flex-grow: ${({flexGrow}) => (flexGrow ? 1 : 'initial')};
+  padding: ${({padding}) => padding};
+  margin: ${({margin}) => margin};
+  cursor: ${({cursor}) => cursor};
+  position: relative;
+`;
+
+// TODO the string type
+interface IFlexItem {
+    flex: number;
+    margin: string;
+    padding: string;
+    width: string;
+}
+
+export const FlexItem = styled.div<IFlexItem>`
+  flex: ${({flex}) => flex};
+  margin: ${({margin}) => margin};
+  padding: ${({padding}) => padding};
+  width: ${({width}) => width};
 `;
