4486d88bae5cb48dab18e273edcbd272941aced9
Mgrdich <mgotm13@gmail.com>
2022-02-21 23:05:38 +0400

    Creating Db connection class function

--- util/DB.ts
+++ util/DB.ts
@@ -1,3 +1,32 @@
+import {MongoClient} from "mongodb";
+
 export default class DB {
 
+    private static _instance: DB;
+
+    private dataBaseStatus: boolean = false;
+
+    private db_connection: any;
+
+    private constructor() {
+        MongoClient.connect(process.env.MONGODB_URI as string).then((db) => {
+            this.db_connection = db;
+        }).catch((err) => {
+            console.log(err);
+            this.dataBaseStatus = false;
+        });
+    }
+
+    public static get Instance() {
+        if (this._instance) {
+            return this._instance;
+        }
+
+        this._instance = new DB();
+        return this._instance;
+    }
+
+    public get db() {
+        return this.db_connection;
+    }
 }
