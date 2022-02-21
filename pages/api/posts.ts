80acf4574981d154a3a493546aae8ed7cc8ba12c
Mgrdich <mgotm13@gmail.com>
2022-02-22 00:21:01 +0400

    Adding get and post functions for the post api

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,9 +1,33 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 import {API_Response} from "../../types/global";
+import DB from "../../util/DB";
+import {Db, InsertOneResult} from "mongodb";
+import Utils from "../../util/Utils";
 
-async function postsPostHandler(req: NextApiRequest, res: NextApiResponse<API_Response>) {}
+async function postsPostHandler(req: NextApiRequest, res: NextApiResponse<API_Response>) {
+    try {
+        // it caches the connection so no worries :)
+        let connection: DB = await DB.Instance();
 
-async function postsGetHandler(req: NextApiRequest, res: NextApiResponse<API_Response>) {}
+        let db: Db = connection.value;
+
+    } catch (err) {
+        return Utils.errorReturn(res);
+    }
+
+}
+
+async function postsGetHandler(req: NextApiRequest, res: NextApiResponse<API_Response>) {
+    try {
+        // it caches the connection so no worries :)
+        let connection: DB = await DB.Instance();
+
+        let db: Db = connection.value;
+
+    } catch (err) {
+        return Utils.errorReturn(res);
+    }
+}
 
 export default function handler(
     req: NextApiRequest,
