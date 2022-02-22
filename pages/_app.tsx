bd2b39610ca7059ae8b02264ee8c6dd20ac6dd3b
Mgrdich <mgotm13@gmail.com>
2022-02-23 00:56:43 +0400

    Adding head to the my app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,15 @@ import '../styles/index.css';
 import type {AppProps} from 'next/app'
 import {ReactNode} from "react";
 import Layout from "../components/layout/Layout";
+import Head from 'next/head';
 
 function MyApp({Component, pageProps}: AppProps): ReactNode {
     return (
       <Layout>
+          <Head>
+              <meta name="viewport" content="width=device-width, initial-scale=1"/>
+              <title>My Blog</title>
+          </Head>
           <Component {...pageProps} />
       </Layout>
     );
